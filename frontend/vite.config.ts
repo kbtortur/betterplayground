f618b96ea33f46abe15d127e8f9a9de71187b35b
vaaski <admin@vaa.ski>
2023-12-24 21:22:51 +0100

    add unplugin-auto-import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import AutoImport from "unplugin-auto-import/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    assetsDir: 'static'
+    assetsDir: "static",
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: true,
+      dirs: ["./src/composables/*", "./src/stores/*"],
+      imports: ["vue", "vue-router", "@vueuse/core"],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
