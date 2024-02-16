c8f143bdf1955fd09771ab1b2a2acd2ccc48aeef
vaaski <admin@vaa.ski>
2024-02-16 13:57:24 +0100

    scale navbar text

--- src/components/main-header.vue
+++ src/components/main-header.vue
@@ -4,15 +4,22 @@ import { routeList } from "@/router"
 
 <template>
   <header>
-    <RouterLink v-for="view in routeList" :to="view.path" :key="view.path">{{
-      view.name
-    }}</RouterLink>
+    <RouterLink v-for="view in routeList" :to="view.path" :key="view.path">
+      {{ view.name }}
+    </RouterLink>
   </header>
 </template>
 
 <style scoped>
 a {
-  margin-right: 1rem;
+  margin-right: 0.5em;
   font-size: 1.5rem;
+  text-wrap: nowrap;
+}
+
+@media screen and (max-width: 750px) {
+  a {
+    font-size: 1rem;
+  }
 }
 </style>
