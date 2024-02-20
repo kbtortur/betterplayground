6048e5f9e92e959a22cdc84345322c0d17ca2d8e
vaaski <admin@vaa.ski>
2024-02-20 14:31:51 +0100

    fix b64ImageToBlob

--- src/composables/image-blob.ts
+++ src/composables/image-blob.ts
@@ -1,25 +1,6 @@
-export const b64ImageToBlob = async (
-  b64Data: string,
-  contentType = "image/png",
-  sliceSize = 512
-) => {
-  const byteCharacters = atob(b64Data)
-  const byteArrays = []
-
-  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    const slice = byteCharacters.slice(offset, offset + sliceSize)
-
-    const byteNumbers = Array.from({ length: slice.length })
-    for (let index = 0; index < slice.length; index++) {
-      byteNumbers[index] = slice.codePointAt(index)
-    }
-
-    const byteArray = new Uint8Array(byteNumbers.length)
-    byteArrays.push(byteArray)
-  }
-
-  const blob = new Blob(byteArrays, { type: contentType })
-  return blob
+export const b64ImageToBlob = async (b64Data: string, contentType = "image/png") => {
+  const response = await fetch(`data:${contentType};base64,${b64Data}`)
+  return await response.blob()
 }
 
 export const urlImageToBlob = async (url: string) => {
